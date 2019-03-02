791870ad2e102de6159e4f17e4446148f083a34d
David Mullins <Mullins6323@gmail.com>
2019-03-02 12:03:21 -0600

    added dogBreed function

--- main.js
+++ main.js
@@ -4,17 +4,17 @@
 // Sam Smith
 // John Doe
 
-const firstName = 'David';
-const lastName = 'Mullins'
-console.log(`${firstName} ${lastName}`);
+// const firstName = 'David';
+// const lastName = 'Mullins'
+// console.log(`${firstName} ${lastName}`);
 
-const firstName1 = 'Britney';
-const lastName1 = 'Spears';
+// const firstName1 = 'Britney';
+// const lastName1 = 'Spears';
 
-console.log(`${firstName1} ${lastName1}`);
+// console.log(`${firstName1} ${lastName1}`);
 
-const firstName2 = 'Tori';
-const lastName2 = 'Amos';
+// const firstName2 = 'Tori';
+// const lastName2 = 'Amos';
 
 const namePrinter = (firstName, lastName) =>{
     console.log(`${firstName} ${lastName}`);
@@ -22,3 +22,16 @@ const namePrinter = (firstName, lastName) =>{
 
 namePrinter('David', 'Mullins');
 namePrinter('John', 'Doe');
+
+
+const nuggetizer = (animal) => {
+    return `processed ${animal}`;
+};
+
+console.log(nuggetizer('pig'));
+
+const dogBreed = (breed) =>{
+    return `my favorite dog breed is ${breed}`;
+};
+
+console.log(dogBreed('border collie'));
