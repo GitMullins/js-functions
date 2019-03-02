95a9fe86847dd7a7eebc2f43bd62557f73fffe22
David Mullins <Mullins6323@gmail.com>
2019-03-02 14:07:13 -0600

    added band function

--- main.js
+++ main.js
@@ -35,3 +35,34 @@ const dogBreed = (breed) =>{
 };
 
 console.log(dogBreed('border collie'));
+
+
+
+const dogBreedDiv = document.getElementById('dog-breeds');
+dogBreedDiv.innerHTML = dogBreed('lab');
+
+const nuggetizerDiv = document.getElementById('animal');
+nuggetizerDiv.innerHTML = nuggetizer('chiKn');
+nuggetizerDiv.innerHTML = nuggetizer('kitten');
+
+const printToDom = (divId, textToPrint) =>{
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+printToDom('dog-breeds', dogBreed('lab'));
+printToDom('animal', nuggetizer('chiKn'));
+printToDom('animal', nuggetizer('kitten'));
+printToDom('animal', ' mmmmmmmmm');
+
+
+let bandNumber = 1;
+const addBand = (bandName) => {
+    //do stuff
+    const bandText = `<h5>${bandNumber}. ${bandName}</h5>`;
+    bandNumber += 1
+    printToDom('band-list', bandText);
+};
+
+addBand('Metalica') // 1. Metalica
+addBand('Slayer')   // 2. Slayer
