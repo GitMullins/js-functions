6ef1932b3dc8a44a10d9e10175ed2c865e6bda74
David Mullins <Mullins6323@gmail.com>
2019-03-02 11:07:23 -0600

    namePrinter function works

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("hi");
+// David Mullins
+// Britney Spears
+// Tori Amos
+// Sam Smith
+// John Doe
+
+const firstName = 'David';
+const lastName = 'Mullins'
+console.log(`${firstName} ${lastName}`);
+
+const firstName1 = 'Britney';
+const lastName1 = 'Spears';
+
+console.log(`${firstName1} ${lastName1}`);
+
+const firstName2 = 'Tori';
+const lastName2 = 'Amos';
+
+const namePrinter = (firstName, lastName) =>{
+    console.log(`${firstName} ${lastName}`);
+};
+
+namePrinter('David', 'Mullins');
+namePrinter('John', 'Doe');
